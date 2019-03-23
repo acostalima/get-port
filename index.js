2ad699792d1c41cb4b22bccc2ffcb2418c9ad801
André Costa Lima <andreclima.pt@gmail.com>
2019-03-23 11:44:09 +0000

    wip

--- index.js
+++ index.js
@@ -28,8 +28,22 @@ const getPort = options => {
 	);
 };
 
+const fromRange = (from, to) => {
+	if (from < 1024) {
+		throw new RangeError(`Lowest port range limit must not be below 1024. Received: ${from}`);
+	}
+
+	if (to - from <= 0) {
+		throw new RangeError(`Port range upper limit must not be lower than the lowest one. Received: [${from}, ${to}]`);
+	}
+
+	const range = Array.from({ length: to - from }, (_, i) => i + from);
+
+	return getPort({ port: range });
+}
+
 module.exports = options => options ?
 	getPort(options).catch(() => getPort(Object.assign(options, {port: 0}))) :
 	getPort({port: 0});
-
+module.exports.fromRange = fromRange;
 module.exports.default = module.exports;
